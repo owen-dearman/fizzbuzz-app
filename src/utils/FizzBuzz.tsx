99374fc37924ed7449ffca461d52047db8faf6d1
Owen Dearman <owendearman@gmail.com>
2022-03-10 08:40:55 +0000

    updated to FizzBuzz

---
+++ src/utils/FizzBuzz.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import fizzBuzzLogic from "./fizzbuzzLogic";
+
+function FizzBuzz(): JSX.Element {
+  const [currentNumber, queueRenderForNextNumber] = useState(1);
+  const [fizzBuzzState, logNextFizzBuzzNumber] = useState<(number | string)[]>(
+    []
+  );
+
+  //adds one to current number and adds corresponding FizzBuzz state to storage
+  const addNext = () => {
+    queueRenderForNextNumber(currentNumber + 1);
+  const state = fizzBuzzLogic(currentNumber)
+    logNextFizzBuzzNumber((prev) => [...prev, state]);
+  };
+
+  //maps storage to 
+  const fizzBuzzLayout: JSX.Element[] = fizzBuzzState.map(fbStorage)
+
+  return (
+    <>
+      <h1>Let's Play FizzBuzz</h1>
+      <p>Just click next to reveal the number</p>
+      <ul style={{listStyleType: "none"}}>{fizzBuzzLayout}</ul>
+      <button onClick={addNext}>NEXT</button>
+    </>
+  );
+}
+
+
+function fbStorage (number:string|number):JSX.Element {
+    return(
+        <>
+            <li style={{fontSize: 20,}}>{number}</li>
+        </>
+    )
+  }
+
+
+export default FizzBuzz;
---
+++ src/utils/fizzbuzzLogic.ts
@@ -0,0 +1,13 @@
+function fizzbuzzLogic(currentState: number):number|string{
+    if (currentState % 5 === 0 && currentState % 3 === 0){
+        return "FizzBuzz"
+    }else if (currentState % 3 ===0){
+        return "Fizz"
+    }else if (currentState % 5 ===0){
+        return "Buzz"
+    }else{
+        return currentState
+    }
+}
+
+export default fizzbuzzLogic
